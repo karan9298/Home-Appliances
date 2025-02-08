29d0fcac5f47861cba11796ce579b2433cdc3243
KARAN PATIL <karanpatil.9298@gmail.com>
2025-02-08 18:33:48 +0530

    Working
    Working

--- Frontend/home/src/pages/Counter.js
+++ Frontend/home/src/pages/Counter.js
@@ -3,10 +3,10 @@ import WhistleCounter from '../components/WhistleCounter';
 import Notification from '../components/Notification';
 
 const Counter = () => {
-  const predefinedCount = 3; // Set your predefined count here
+  const predefinedCount = 3; // Set predefined count to 3
 
   const handleNotification = () => {
-    const audio = new Audio('/path/to/notification-sound.mp3');
+    const audio = new Audio('/Audios/cheerful-happy-cooking-food-music-244393.mp3'); // Update the path to the audio file
     audio.play();
   };
 
